fe796eda7f9a0d5947063428c1c9a0da4d5293ad
pamarovis <hidovydas@gmail.com>
2025-01-27 21:06:06 +0200

    pataisytas test failas

--- test_app.py
+++ test_app.py
@@ -1,9 +1,9 @@
 # Paprasciausias demo testas patikrinti veikima
 import subprocess
 
-def test_hello_world_in_output():
+def test_labas_in_output():
     # Paleidžiame skriptą
     result = subprocess.run(["python", "app.py"], capture_output=True, text=True)
     
     # Tikriname ar yra žodis Hello
-    assert "Hello" in result.stdout
+    assert "Labas" in result.stdout
